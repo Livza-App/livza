583d0422dc74067e2e16cd5fddaac187f9ed7783
Abdou_Ch <reus07abdo@gmail.com>
2020-12-02 14:27:37 +0100

    Login_signup

---
+++ app/src/main/java/com/example/livza/Log_In.java
@@ -0,0 +1,14 @@
+package com.example.livza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Log_In extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log__in);
+    }
+}
