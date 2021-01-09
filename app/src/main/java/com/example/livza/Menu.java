45ca3bc8dfffc8e4660a681cb0e5fbd9e497745b
AbdalghaniFeddak <68193894+AbdalghaniFeddak@users.noreply.github.com>
2021-01-09 15:27:14 +0100

    menuDrawer

--- app/src/main/java/com/example/livza/Menu.java
+++ app/src/main/java/com/example/livza/Menu.java
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
@@ -18,18 +19,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -43,6 +52,8 @@ public class Menu extends AppCompatActivity implements CatitemAdapter.Oncategori
     private ArrayList<Categorieitem> categories;
     private ArrayList<String> food_key;
     private DatabaseReference mReference;
+    private FirebaseUser mUser;
+    private FirebaseStorage mStorage;
     public static int cat_pos=0;
     private Long firstCatNumber;
     private CatitemAdapter catadapter;
@@ -54,6 +65,9 @@ public class Menu extends AppCompatActivity implements CatitemAdapter.Oncategori
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     private Toolbar toolbar;
+    private View header;
+    private ImageView headerImage;
+    private TextView headerUsername,headerPhone;
     public static ArrayList<Carte_item> cart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,6 +114,38 @@ public class Menu extends AppCompatActivity implements CatitemAdapter.Oncategori
 
 
     private void initFirebase(){
+
+        mReference.child("Users").child(mUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user=snapshot.getValue(User.class);
+                headerUsername.setText(user.getUsername());
+                headerPhone.setText(user.getPhone());
+                mStorage=FirebaseStorage.getInstance();
+                StorageReference mStorageRef=mStorage.getReference().child("/Users").child("/"+user.getImageID());
+                mStorageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Glide.with(getApplicationContext())
+                                .load(uri)
+                                .into(headerImage);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle any errors
+                        Glide.with(getApplicationContext())
+                                .load(R.drawable.addtocart_cochetrue)
+                                .into(headerImage);
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         firstTimeEvent=new ValueEventListener() {
             @Override
@@ -236,6 +282,15 @@ public class Menu extends AppCompatActivity implements CatitemAdapter.Oncategori
         menu=findViewById(R.id.menuitems);
         drawerLayout=findViewById(R.id.drawer_layout);
         navigationView=findViewById(R.id.nav_view);
+        header= navigationView.getHeaderView(0);
+        headerUsername=header.findViewById(R.id.header_username);
+        headerPhone=header.findViewById(R.id.header_phone);
+        headerImage=header.findViewById(R.id.header_image);
+
+
+        //Firebase
+        mUser= FirebaseAuth.getInstance().getCurrentUser();
+        mStorage=FirebaseStorage.getInstance();
 
         //Arraylist
         foods=new ArrayList<>();
