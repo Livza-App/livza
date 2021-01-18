56e7571ed649bff56700622337e105907c800527
Abdou_Ch <reus07abdo@gmail.com>
2021-01-18 21:48:01 +0100

    UI_setUp

--- app/src/main/java/com/example/livza/Profile_User.java
+++ app/src/main/java/com/example/livza/Profile_User.java
@@ -96,14 +96,14 @@ public class Profile_User extends AppCompatActivity {
 
         //Back btn
         Button back = findViewById(R.id.btn_back);
-        back.setOnClickListener(new View.OnClickListener() {
+        /*back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 /*Intent b = new Intent(Profile_User.this, Menu.class);
-                startActivity(b);*/
+                startActivity(b);
                 finish();
             }
-        });
+        });*/
 
 
     }
