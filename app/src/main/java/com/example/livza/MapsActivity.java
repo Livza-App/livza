40d198be0177c1bc953522acaa7fc6012b814c1a
AbdalghaniFeddak <68193894+AbdalghaniFeddak@users.noreply.github.com>
2021-01-28 17:40:32 +0100

    markers

--- app/src/main/java/com/example/livza/MapsActivity.java
+++ app/src/main/java/com/example/livza/MapsActivity.java
@@ -1,26 +1,36 @@
 package com.example.livza;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.model.RectangularBounds;
@@ -42,22 +52,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private EditText mSearchText;
 
     private GoogleMap mMap;
-    private PlaceAutocompleteFragment placeAutoComplete;
-    private AutocompleteSupportFragment autocompleteFragment;
-    private String apiKey;
-    private PlacesClient placesClient;
+    private View mapView;
     public static String TAG = "Maps";
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LatLng markerPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        apiKey = getString(R.string.google_maps_key);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mapView = mapFragment.getView();
+
     }
 
     /**
@@ -73,9 +84,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         init();
+
+        View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+        RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+        // position on right bottom
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+        rlp.setMargins(0, 180, 180, 0);
+        //rlp.addRule(RelativeLayout.BELOW, R.id.activity_maps_searchcard);
+
+        //
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                MarkerOptions markerOptions=new MarkerOptions();
+                markerOptions.position(latLng);
+                markerPos=latLng;
+                markerOptions.title("you choosed this position");
+                mMap.clear();
+                mMap.addMarker(markerOptions);
+            }
+        });
+
+        //correct location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+
+                        LatLng user_location = new LatLng(location.getLatitude(), location.getLongitude());
+
+
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(user_location));
+                        CameraPosition cameraPosition = new CameraPosition.Builder()
+                                .target(user_location)      // Sets the center of the map to user_location
+                                .zoom(10)                   // Sets the zoom to 15 (city zoom)
+                                .build();                   // Creates a CameraPosition from the builder
+                        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+
+                    }
+                }
+            });
+            mMap.setMyLocationEnabled(true);
+        }
     }
 
     private void init() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        //view
         mSearchText = findViewById(R.id.activity_maps_searchadresse);
         mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -93,21 +152,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void search() {
-        String searchText=mSearchText.getText().toString();
-        Geocoder geocoder=new Geocoder(MapsActivity.this);
-        List<Address> list=new ArrayList<>();
+        String searchText = mSearchText.getText().toString();
+        Geocoder geocoder = new Geocoder(MapsActivity.this);
+        List<Address> list = new ArrayList<>();
         try {
-            list=geocoder.getFromLocationName(searchText,1);
+            list = geocoder.getFromLocationName(searchText, 1);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if(list.size()>0){
-            Address address=list.get(0);
+        if (list.size() > 0) {
+            Address address = list.get(0);
+            //TODO : search cantry
             moveCamera(address);
         }
     }
 
-    private void moveCamera(Address address){
+    private void moveCamera(Address addressSearch) {
+        LatLng adresse = new LatLng(addressSearch.getLatitude(), addressSearch.getLongitude());
+        markerPos=adresse;
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(adresse)      // Sets the center of the map to Mountain View
+                .zoom(17)                   // Sets the zoom
+                .tilt(30)
+                // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        MarkerOptions markerOptions=new MarkerOptions();
+        markerOptions.position(adresse);
+        markerOptions.title("you choosed this position");
+        mMap.clear();
+        mMap.addMarker(markerOptions);
 
     }
 
